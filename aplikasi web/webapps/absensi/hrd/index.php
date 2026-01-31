eef218ae483a28670bc47f47e8d6b43d14147ea0
ichsanleonhart <39687405+ichsanleonhart@users.noreply.github.com>
2026-02-01 01:16:02 +0700

    bot whatsapp auto japri pegawai lupa absen masuk / pulang

--- aplikasi web/webapps/absensi/hrd/index.php
+++ aplikasi web/webapps/absensi/hrd/index.php
@@ -137,6 +137,16 @@ $nama_hrd = $cek_pegawai ? $cek_pegawai['nama'] : $nik_hrd;
                     
                 </p>
             </a>
+			
+			<a href="auto_reminder.php" class="block bg-gray-800 hover:bg-gray-700 p-5 rounded-xl border border-gray-700 transition shadow hover:shadow-lg group relative overflow-hidden">
+                <div class="w-10 h-10 bg-red-900/50 rounded-lg flex items-center justify-center text-red-400 mb-3 group-hover:text-white group-hover:bg-red-600 transition">
+                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path></svg>
+                </div>
+                <h3 class="font-bold text-white group-hover:text-red-300">Bot Whatsapp Auto Reminder</h3>
+                <p class="text-xs text-gray-400 mt-1">Otomatis japri pegawai lupa absen
+                    
+                </p>
+            </a>
             
         </div>
     </div>
