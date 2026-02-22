2f93c26ab11689dca88f534632fb54e01d64334e
ichsanleonhart <39687405+ichsanleonhart@users.noreply.github.com>
2026-02-22 10:40:46 +0700

    perbaikan config aplikasi mpp agar jalan di reverse proxy

--- aplikasi web/mpp/config/config.php
+++ aplikasi web/mpp/config/config.php
@@ -1,15 +1,12 @@
 <?php
 // File: config/config.php
 
-// 1. Deteksi Base URL otomatis (Untuk Dashboard MPP sendiri)
-// Ini boleh dinamis karena Dashboardnya ada di laptopmu.
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://";
-$base_url = $protocol . $_SERVER['HTTP_HOST'] . "/mpp2/";
+// 1. Deteksi Base URL otomatis (Anti-Reverse Proxy / Protocol-Relative)
+// Gunakan awalan "//" agar browser otomatis mengikuti HTTP atau HTTPS yang sedang aktif!
+$base_url = "//" . $_SERVER['HTTP_HOST'] . "/edokter/";
 
 // 2. Konfigurasi Lokasi Webapps (SUMBER GAMBAR KHANZA)
-// PENTING: Jangan pakai logika if localhost. Tembak langsung ke IP Server Khanza.
-// IP ini adalah lokasi fisik dimana folder "webapps/radiologi" berada.
-$webapps_url = "https://aplikasi.rssuliawati.com/webapps/"; 
+$webapps_url = "https://aplikasi.klinikgmc.my.id/webapps/";
 
 // 3. Path Absolut
 define('BASE_PATH', dirname(__DIR__) . '/');
